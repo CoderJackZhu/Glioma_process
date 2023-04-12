bd2d13956f4f744b7e669f6f0e6700b6239bf607
spgou <zhuyijie151@sina.com>
2023-04-12 22:18:27 +0800

    update fsl file

--- 6fsl_skull-strip.py
+++ 6fsl_skull-strip.py
@@ -4,12 +4,14 @@ import pandas as pd
 from tqdm import tqdm
 import numpy as np
 import SimpleITK as sitk
-
-def sef
+import xlrd
+def select_after_operation():
+    operation_info = xlrd.open_workbook(r"./reference/12321321.xls")
+    print(operation_info[0])
 
 
 def skull_strip(registrated_dir, skull_stripped_dir):
-
+    pass
 
 
 
@@ -17,8 +19,9 @@ def skull_strip(registrated_dir, skull_stripped_dir):
 
 
 if __name__ == "__main__":
-    registrated_dir = r".\Registrated"
-    skull_stripped_dir = r".\skull_stripped"
+    registrated_dir = r"./Registrated"
+    skull_stripped_dir = r"./skull_stripped"
     if not os.path.exists(skull_stripped_dir):
         os.mkdir(skull_stripped_dir)
+    select_after_operation()
     skull_strip(registrated_dir, skull_stripped_dir)
