66ed0c72a24f1c8a68fe4a0bbe051bed654c4003
YIjie Zhu <42195616+CoderJackZhu@users.noreply.github.com>
2023-04-13 11:36:28 +0800

    update fsl file skull

--- 6fsl_skull-strip.py
+++ 6fsl_skull-strip.py
@@ -12,16 +12,27 @@ def check_if_operation(patient_id, check_date):
         id = operation_info.iloc[i, 0]
         if id == patient_id:
             if operation_info.iloc[i, 1] == 'nan':
-                return 'nan'
+                return False
             operation_data = operation_info.iloc[i, 1].replace('-', '')
             if check_date < operation_data:
-                return 'pre'
+                return True
             else:
-                return 'post'
+                return False
 
 
 def skull_strip(registrated_dir, skull_stripped_dir):
-    pass
+    for patient in tqdm(os.listdir(registrated_dir)):
+        patient_id = patient.split('_')[0]
+        patient_date = patient.split('_')[1]
+        if_operation = check_if_operation(patient_id, patient_date)
+        if not if_operation:
+            continue
+        patient_dir = os.path.join(registrated_dir, patient)
+        if not os.path.exists(os.path.join(skull_stripped_dir, patient)):
+            os.mkdir(os.path.join(skull_stripped_dir, patient))
+        for modality in os.listdir(patient_dir):
+            modality_file = os.path.join(patient_dir, modality)
+
 
 
 if __name__ == "__main__":
